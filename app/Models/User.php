74f94fda938d8b69f5b289b5f96ce55e15d3ee51
depcito <samuelh.echeverri@outlook.com>
2026-09-22 10:13:38 -0500

    User model with typed getters/setters, address attribute and seeding

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,21 +8,27 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * User model.
+ *
+ * Attributes:
+ * - id: int
+ * - name: string
+ * - email: string
+ * - password: string
+ * - address: string
+ */
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
     /**
-     * The attributes that are mass assignable.
+     * The attributes that aren't mass assignable.
      *
      * @var list<string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $guarded = ['id'];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -45,5 +51,85 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Get the user identifier.
+     */
+    public function getId(): int
+    {
+        return $this->attributes['id'];
+    }
+
+    /**
+     * Set the user identifier.
+     */
+    public function setId(int $id): void
+    {
+        $this->attributes['id'] = $id;
+    }
+
+    /**
+     * Get the user name.
+     */
+    public function getName(): string
+    {
+        return $this->attributes['name'];
+    }
+
+    /**
+     * Set the user name.
+     */
+    public function setName(string $name): void
+    {
+        $this->attributes['name'] = $name;
+    }
+
+    /**
+     * Get the user email.
+     */
+    public function getEmail(): string
+    {
+        return $this->attributes['email'];
+    }
+
+    /**
+     * Set the user email.
+     */
+    public function setEmail(string $email): void
+    {
+        $this->attributes['email'] = $email;
+    }
+
+    /**
+     * Get the user password (hashed).
+     */
+    public function getPassword(): string
+    {
+        return $this->attributes['password'];
+    }
+
+    /**
+     * Set the user password.
+     */
+    public function setPassword(string $password): void
+    {
+        $this->attributes['password'] = $password;
+    }
+
+    /**
+     * Get the user address.
+     */
+    public function getAddress(): ?string
+    {
+        return $this->attributes['address'] ?? null;
+    }
+
+    /**
+     * Set the user address.
+     */
+    public function setAddress(?string $address): void
+    {
+        $this->attributes['address'] = $address;
     }
 }
